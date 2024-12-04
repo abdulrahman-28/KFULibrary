55f5a4650398db37dc2c7a0ff41b5a32252d3d25
Abdulrahman Mohammed <alsarawiam@gmail.com>
2024-12-04 16:13:08 +0300

    register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,15 +4,21 @@ export default function page() {
       <section className="flex-1 flex items-center justify-center">
         <form className="flex flex-col gap-4">
           <h1 className="text-3xl font-bold">KFU Library</h1>
+          <input type="text" placeholder="Username" className="input" />
           <input type="text" placeholder="Email" className="input" />
           <input type="password" placeholder="Password" className="input" />
+          <input
+            type="password"
+            placeholder="Retype your password"
+            className="input"
+          />
           <a className="btn rounded-full" href={"/"}>
-            Login
+            Create Acoount
           </a>
           <p>
-            Don't have an accout?{" "}
-            <a className="text-stone-500 hover:text-stone-700" href="/register">
-              Create One
+            already have an accout?{" "}
+            <a className="text-stone-500 hover:text-stone-700" href="/login">
+              Login
             </a>
           </p>
         </form>
